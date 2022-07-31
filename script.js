5f0ead1d6aea34cffe39a90d0b4dc535432f1c6c
Kamran <kamran-anwar99@hotmail.com>
2022-07-31 15:57:40 +0100

    Player can place 'X' or 'O' on the grid

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
 const cellElements = document.querySelectorAll('[data-cell');
+const board = document.getElementById('board');
 
-cellElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, { once: true })
-})
+let circleTurn;
+
+startGame();
+
+function startGame() {
+    circleTurn = false;
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true })
+    })
+    setBoardHoverClass();
+}
 
 
 function handleClick(e) {
-    //placeMark
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    placeMark(cell, currentClass)
+
+    placeMark(cell, currentClass)
     //Check for Win
     //Check for Draw
     //Switch Turns
+    swapTurns();
+    setBoardHoverClass();
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+
+function swapTurns() {
+    circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS);
+    } else {
+        board.classList.add(X_CLASS);
+    }
 }
